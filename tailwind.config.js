41625a1cd202fbbbb84f31fdd9f454d6054f26e8
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2024-10-01 00:47:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        grey_primary: "#4E4E4E",
+        grey_secondary: "#969696"
+      }
+    },
+  },
+  plugins: [],
+}
+
