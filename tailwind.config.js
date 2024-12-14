56127eb804f5e2a7631257a10012d1fb8bdd206f
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2024-12-15 02:41:38 +0700

    add gear page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,11 @@ module.exports = {
       colors: {
         grey_primary: "#4E4E4E",
         grey_secondary: "#969696"
-      }
+      },
+      boxShadow: {
+        'custom': 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+      },
     },
   },
   plugins: [],
 }
-
